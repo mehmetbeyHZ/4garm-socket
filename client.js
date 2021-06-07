ad4f7e18969a910f18c8494b78aea43ac343ccf6
mehmetbeyhz <mehmetbeyhz@gmail.com>
2021-06-07 13:29:42 +0300

    test commit

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 var serialport = require('serialport');
-var portName = '/dev/tty.usbserial-110';
+var portName = '/dev/ttyUSB0';
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -8,29 +8,34 @@ const port = 3133
 // CommonJS
 const io = require("socket.io-client");
 
-const socket = io("http://127.0.0.1:3132/");
+const socket = io("http://195.201.168.87:3132/");
+
+
+
+
+var myPort = new serialport(portName, {
+    baudRate: 9600,
+    parser: new serialport.parsers.Readline('\n')
+});
+
+
+myPort.on('open', onOpen);
+myPort.on('data', onData);
+
+function onOpen(){
+    console.log('Open connections!');
+}
+
+function onData(data){
+    console.log('on Data ' + data);
+}
 
 
 socket.on('robotic.data', data => {
-    console.log(data);
+    myPort.write(data)
+    console.log(data)
 })
 
-// var myPort = new serialport(portName, {
-//     baudRate: 9600,
-//     parser: new serialport.parsers.Readline('\n')
-// });
-//
-//
-// myPort.on('open', onOpen);
-// myPort.on('data', onData);
-
-// function onOpen(){
-//     console.log('Open connections!');
-// }
-//
-// function onData(data){
-//     console.log('on Data ' + data);
-// }
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -42,6 +47,9 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html')
 })
 
+// setInterval(function (){
+//     myPort.write("DOWNUP:500!")
+// },1000)
 
 // app.post('/router',(req,res) => {
 //     const {route_name, value} = req.body;
